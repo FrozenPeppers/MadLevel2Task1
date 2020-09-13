02dd495ce54ce21188d55e1ac9caab9714b88d3b
frozenpeppers <joris-ebbelaar@hotmail.nl>
2020-09-13 09:13:50 +0200

    implemented PlaceAdapter + binding in ImageListActivity(main)

--- app/src/main/java/com/example/madlevel2task1/ImageListActivity.kt
+++ app/src/main/java/com/example/madlevel2task1/ImageListActivity.kt
@@ -3,20 +3,33 @@ package com.example.madlevel2task1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.madlevel2task1.adapter.PlaceAdapter
 import com.example.madlevel2task1.databinding.ActivityImageListBinding
+import com.example.madlevel2task1.model.PlaceModel
+import kotlinx.android.synthetic.main.activity_image_list.*
 
 class ImageListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityImageListBinding
+
+    private val places = arrayListOf<PlaceModel>()
+    private val placeAdapter = PlaceAdapter(places)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImageListBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        initView()
     }
 
-    private fun initView(){
+    private fun initView() {
+        rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        rvPlaces.adapter = placeAdapter
 
+        for (i in PlaceModel.PLACE_NAMES.indices) {
+            places.add(PlaceModel(PlaceModel.PLACE_NAMES[i], PlaceModel.PLACE_RES_DRAWABLE_IDS[i]))
+        }
     }
 }
--- app/src/main/java/com/example/madlevel2task1/adapter/PlaceAdapter.kt
+++ app/src/main/java/com/example/madlevel2task1/adapter/PlaceAdapter.kt
@@ -1,2 +1,44 @@
 package com.example.madlevel2task1.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2task1.R
+import com.example.madlevel2task1.model.PlaceModel
+import kotlinx.android.synthetic.main.item_place.view.*
+
+class PlaceAdapter(private val places: List<PlaceModel>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    lateinit var context: Context
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_place, parent, false))
+    }
+
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(place: PlaceModel) {
+            itemView.ivPlace.setImageDrawable(context.getDrawable(place.imageResId))
+            itemView.tvPlace.text = place.name
+        }
+
+    }
+
+
+}
